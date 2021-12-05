56ec17c9d8a91be2fba026df60dbf3dc84e45eb5
diegomaustem <diego.maustem@gmail.com>
2021-12-05 19:40:46 -0300

    templates-blade

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', HomeController::class)->name('home');
+Route::post('painel',[UsuarioController::class, 'login'])->name('usuarios.login');
